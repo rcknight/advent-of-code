68ec96f6442ce8eea61109c93f4bbf61c50f42cb
Rich Knight <rich@rich-knight.com>
2016-12-07 20:23:17 +0000

    Began #1

---
+++ 1/1.hs
@@ -0,0 +1,22 @@
+module Advent1 where
+
+import Data.List.Split
+
+data Direction = N | S | E | W
+      deriving (Read, Show, Enum, Eq, Ord)
+
+data Turn = R | L
+      deriving (Read, Show, Enum, Eq, Ord)
+
+type Length = Integer
+type Input = (Turn, Length)
+
+parseInput :: String -> Input
+parseInput (d:l) = (read [d], read l)
+
+input = "R1, R1, R3, R1, R1, L2, R5, L2, R5, R1, R4, L2, R3, L3, R4, L5, R4, R4, R1, L5, L4, R5, R3, L1, R4, R3, L2, L1, R3, L4, R3, L2, R5, R190, R3, R5, L5, L1, R54, L3, L4, L1, R4, R1, R3, L1, L1, R2, L2, R2, R5, L3, R4, R76, L3, R4, R191, R5, R5, L5, L4, L5, L3, R1, R3, R2, L2, L2, L4, L5, L4, R5, R4, R4, R2, R3, R4, L3, L2, R5, R3, L2, L1, R2, L3, R2, L1, L1, R1, L3, R5, L5, L1, L2, R5, R3, L3, R3, R5, R2, R5, R5, L5, L5, R2, L3, L5, L2, L1, R2, R2, L2, R2, L3, L2, R3, L5, R4, L4, L5, R3, L4, R1, R3, R2, R4, L2, L3, R2, L5, R5, R4, L2, R4, L1, L3, L1, L3, R1, R2, R1, L5, R5, R3, L3, L3, L2, R4, R2, L5, L1, L1, L5, L4, L1, L1, R1"
+
+parsed :: [Input]
+parsed = map parseInput processed
+    where
+        processed = split (condense . dropDelims $ oneOf ", ")
