3933accc1ab42083095885bf6a9c583ed1c0315f
Rich Knight <rich@rich-knight.com>
2016-12-08 21:44:17 +0000

    Day 5 implemented

---
+++ 5/5.hs
@@ -0,0 +1,39 @@
+module Advent5 where
+
+import Crypto.Hash
+import Data.ByteString (ByteString) 
+import Data.String (fromString)
+import Data.Char (isNumber)
+import Data.List (isPrefixOf)
+
+input = "cxdnnyjw"
+
+hexMD5 :: String -> String
+hexMD5 = show . md5 . fromString
+  where
+      md5 :: ByteString -> Digest MD5
+      md5 = hash
+
+generateHashes :: String -> [String]
+generateHashes seed = [ hexMD5 (seed ++ show i) | i <- [0..] ]
+
+updatePassword :: String -> (Int,Char) -> String
+updatePassword existing (pos,new)
+    | pos >= (length existing)  = existing
+    | (existing !! pos) /= '_'  = existing
+    | otherwise                 = (take pos existing) ++ [new] ++ (drop (pos+1) existing)
+
+part1 :: String -> String
+part1 seed = 
+            map (!! 5)
+            $ take 8
+            $ filter (isPrefixOf "00000")
+            $ generateHashes seed
+
+part2 :: String -> [String]
+part2 seed =
+            scanl updatePassword "________"
+            $ map (\x -> (read [x !! 5], x !! 6))
+            $ filter (\x -> isNumber (x !! 5))
+            $ filter (isPrefixOf "00000")
+            $ generateHashes seed
