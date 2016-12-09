c8644978fcbfd2003e215a2bae0b990e953b662b
Rich Knight <rich@rich-knight.com>
2016-12-09 11:45:54 +0000

    Day 6 part 2

--- 6/6.hs
+++ 6/6.hs
@@ -2,15 +2,22 @@ module Advent6 where
 
 import Data.List
 
+transform :: (String -> String -> Ordering) -> String -> String
+transform orderBy fileContents =
+    map head
+    $ map (maximumBy orderBy) 
+    $ map group
+    $ map sort
+    $ transpose
+    $ lines
+    $ fileContents
+
+
 part1 = do
     contents <- readFile "day6input.txt"
-    let transformed = 
-            map head
-            $ map (maximumBy (\g1 g2 -> compare (length g1) (length g2)))
-            $ map group
-            $ map sort
-            $ transpose
-            $ lines
-            $ contents
+    print $ transform (\g1 g2 -> compare (length g1) (length g2)) contents
 
-    print $ transformed
+part2 = do
+    contents <- readFile "day6input.txt"
+    print $ transform (\g1 g2 -> compare (length g2) (length g1)) contents
+
